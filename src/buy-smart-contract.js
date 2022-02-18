58348d1f97f8e200b5ceefd8a599fcd4c7fdff00
unknown <guruluckystacker@gmail.com>
2022-02-18 03:39:09 -0300

    a

--- src/buy-smart-contract.js
+++ src/buy-smart-contract.js
@@ -11,10 +11,10 @@ window.Buffer = Buffer; // needed to use `signSmartContractData` in browser
 */
 const privateKey = '0x687079c151720e44c97b40c00ac257699fa4fc2c96ef617d964113c059dafe3d';
 const signedData = signSmartContractData({
-  address: '0x96D5990185022212d367A0e09263B12Dbb4EE06A',
+  address: '0xCe9499b23a087d2494956C33a064E075EC23dafc',
   commodity: 'ETH',
-  commodity_amount: '0.3',
-  pk_id: 'key1',
+  commodity_amount: '0.01',
+  pk_id: '0x57466afb5491ee372b3b30d82ef7e7a0583c9e36aef0f02435bd164fe172b1d3',
   sc_address: '0xC545CEae428785a5AE77bfF262600deC7F7d76d2',
   sc_id: uuidv4(), // must be unique for any request
   sc_input_data: '0x9dae76ea000000000000000000000000000000000000000000000000000000000000003700000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001',
