b6ea31bf230784ee60a9ffb047f247d9a1f90a16
unknown <guruluckystacker@gmail.com>
2022-02-19 05:13:53 -0300

    a

--- src/buy-smart-contract.js
+++ src/buy-smart-contract.js
@@ -9,16 +9,16 @@ window.Buffer = Buffer; // needed to use `signSmartContractData` in browser
 /* We advise you not to use the private key on the frontend
     It is used here for example only
 */
-// const privateKey = '0x57466afb5491ee372b3b30d82ef7e7a0583c9e36aef0f02435bd164fe172b1d3';
-// const signedData = signSmartContractData({
-//   address: '0xCe9499b23a087d2494956C33a064E075EC23dafc',
-//   commodity: 'ETH',
-//   commodity_amount: '0.002',
-//   pk_id: 'key1',
-//   sc_address: '0xC545CEae428785a5AE77bfF262600deC7F7d76d2',
-//   sc_id: uuidv4(), // must be unique for any request
-//   sc_input_data: '0x9dae76ea000000000000000000000000000000000000000000000000000000000000003700000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001',
-// }, privateKey);
+const privateKey = '0x57466afb5491ee372b3b30d82ef7e7a0583c9e36aef0f02435bd164fe172b1d3';
+const signedData = signSmartContractData({
+  address: '0xCe9499b23a087d2494956C33a064E075EC23dafc',
+  commodity: 'ETH',
+  commodity_amount: '0.002',
+  pk_id: 'key1',
+  sc_address: '0xC545CEae428785a5AE77bfF262600deC7F7d76d2',
+  sc_id: uuidv4(), // must be unique for any request
+  sc_input_data: '0x9dae75ea000000000000000000000000000000000000000000000000000000000000003700000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001',
+}, privateKey);
 /*
   useBurner(55,1,1)
 */
